dcb5f32f3ec802a330a4009969d444133a2dc115
Ed <ed-commits@github.com>
2020-05-25 11:17:59 +0100

    more sql commands

--- star_wars.sql
+++ star_wars.sql
@@ -6,6 +6,10 @@ CREATE TABLE characters (
     age INT
 );
 
-INSERT INTO characters (name, darkside, age) VALUES ('luke', TRUE, 22);
+INSERT INTO characters (name, darkside, age) VALUES ('anakin', false, 22);
+
+UPDATE characters SET (name, darkside) = ('darth vader', true) WHERE name = 'anakin';
+
+INSERT INTO characters (name, darkside, age) VALUES ('yoda', false, 999);
 
 SELECT * FROM characters;
