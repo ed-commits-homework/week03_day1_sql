1d8e5520c30d0593fec2bec18c885ae4bd6d5c5f
Ed <ed-commits@github.com>
2020-05-25 14:01:10 +0100

    add a SERIAL primary key

--- star_wars.sql
+++ star_wars.sql
@@ -1,15 +1,19 @@
 DROP TABLE characters;
 
 CREATE TABLE characters (
+    id SERIAL,
     name VARCHAR(255),
     darkside BOOLEAN,
     age INT
 );
 
 INSERT INTO characters (name, darkside, age) VALUES ('anakin', false, 22);
+INSERT INTO characters (name, darkside, age) VALUES ('obi-wan', false, 25);
 
 UPDATE characters SET (name, darkside) = ('darth vader', true) WHERE name = 'anakin';
 
 INSERT INTO characters (name, darkside, age) VALUES ('yoda', false, 999);
+
+UPDATE characters SET (age) = 65 WHERE name = 'obi-wan';
 
 SELECT * FROM characters;
